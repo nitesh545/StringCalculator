c0ac96df5f34686b43b246c6ce46e91bcfcdd81a
nitecse <hitman12457868@gmail.com>
2024-10-18 20:30:44 +0530

    empty string should give sum 0

---
+++ README.md
@@ -0,0 +1,7 @@
+# String calculator in Rust
+
+## Commit 1: first commit
+Created directory and pushed to github.
+
+## Commit 2: empty string should give sum 0
+Empty string as input to my function ```sum()``` should render the output 0.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+fn sum(inputString: String) -> i32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty_string() {
+        assert_eq!(sum(String::new()), 0);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }
