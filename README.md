609584189a8d2a3804ec5aba21fbd92d720cffcb
nitecse <hitman12457868@gmail.com>
2024-10-18 21:15:54 +0530

    string with 1 number

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Created directory and pushed to github.
 
 ## Commit 2: empty string should give sum 0
 Empty string as input to my function ```sum()``` should render the output 0.
+
+### Commit 3: string with 1 number:
+A string like ```"1"``` gives output ```1```.
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-fn sum(inputString: String) -> i32 {
-    0
+fn sum(input_string: String) -> i32 {
+    if input_string.is_empty() {return 0;}
+    input_string.parse::<i32>().expect("Not a number")
 }
 
 #[cfg(test)]
@@ -9,6 +10,11 @@ mod tests {
     #[test]
     fn empty_string() {
         assert_eq!(sum(String::new()), 0);
+    }
+
+    #[test]
+    fn one_number() {
+        assert_eq!(sum(String::from("1")), 1);
     }
 }
 
