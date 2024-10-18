b99cf35dedf845c73837512e9ccd8c60f52c82df
nitecse <hitman12457868@gmail.com>
2024-10-18 21:30:19 +0530

    string upto 2 numbers

--- README.md
+++ README.md
@@ -6,5 +6,8 @@ Created directory and pushed to github.
 ## Commit 2: empty string should give sum 0
 Empty string as input to my function ```sum()``` should render the output 0.
 
-### Commit 3: string with 1 number:
+## Commit 3: string with 1 number
 A string like ```"1"``` gives output ```1```.
+
+## Commit 4: string upto 2 numbers
+A string like ```"1, 4"``` should give output ```5```.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-fn sum(input_string: String) -> i32 {
+fn add(input_string: String) -> i32 {
     if input_string.is_empty() {return 0;}
-    input_string.parse::<i32>().expect("Not a number")
+
+    let splitted_string = input_string.split(',').collect::<Vec<&str>>();
+    let trimmed_string = splitted_string.into_iter().map(|x| x.trim().to_string()).collect::<Vec<String>>();
+    let numbers = trimmed_string.into_iter().map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    let sum = numbers.iter().fold(0, |acc, x| acc + x);
+    sum
 }
 
 #[cfg(test)]
@@ -9,13 +14,19 @@ mod tests {
 
     #[test]
     fn empty_string() {
-        assert_eq!(sum(String::new()), 0);
+        assert_eq!(add(String::new()), 0);
     }
 
     #[test]
     fn one_number() {
-        assert_eq!(sum(String::from("1")), 1);
+        assert_eq!(add(String::from("1")), 1);
     }
+
+    #[test]
+    fn two_numbers() {
+        assert_eq!(add(String::from("1, 4")), 5);
+    }
+
 }
 
 fn main() {
