79c2b67570402df1315be77ae999bd25a069f197
nitecse <hitman12457868@gmail.com>
2024-10-18 21:37:00 +0530

    string with multiple numbers

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ A string like ```"1"``` gives output ```1```.
 
 ## Commit 4: string upto 2 numbers
 A string like ```"1, 4"``` should give output ```5```.
+
+## Commit 5: string with multiple numbers
+A string like ```"1, 4, 2, 8"``` should give output ```15```.<br>
+Test case passed in first run without modifying code. Code from last commit was good enough.
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 fn add(input_string: String) -> i32 {
     if input_string.is_empty() {return 0;}
 
-    let splitted_string = input_string.split(',').collect::<Vec<&str>>();
-    let trimmed_string = splitted_string.into_iter().map(|x| x.trim().to_string()).collect::<Vec<String>>();
-    let numbers = trimmed_string.into_iter().map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
-    let sum = numbers.iter().fold(0, |acc, x| acc + x);
+    let splitted_string: Vec<&str> = input_string.split(',').collect::<Vec<&str>>();
+    let trimmed_string: Vec<String> = splitted_string.into_iter().map(|x| x.trim().to_string()).collect::<Vec<String>>();
+    let numbers: Vec<i32> = trimmed_string.into_iter().map(|x| x.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    let sum: i32 = numbers.iter().fold(0, |acc, x| acc + x);
     sum
 }
 
@@ -27,6 +27,10 @@ mod tests {
         assert_eq!(add(String::from("1, 4")), 5);
     }
 
+    #[test]
+    fn multiple_numbers() {
+        assert_eq!(add(String::from("1, 4, 2, 8")), 15);
+    }
 }
 
 fn main() {
